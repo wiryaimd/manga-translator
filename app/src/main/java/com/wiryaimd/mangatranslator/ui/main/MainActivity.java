ab46d517e90953a37ef6e940e7be83f42a5ccc65
wiryaimd <adnyasutha003@gmail.com>
2021-07-11 19:43:42 +0800

    anjengg stress gw bikin ni app bangsaddd

--- app/src/main/java/com/wiryaimd/mangatranslator/ui/main/MainActivity.java
+++ app/src/main/java/com/wiryaimd/mangatranslator/ui/main/MainActivity.java
@@ -11,10 +11,19 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.graphics.pdf.PdfRenderer;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
@@ -24,13 +33,17 @@ import com.bumptech.glide.Glide;
 import com.wiryaimd.mangatranslator.R;
 import com.wiryaimd.mangatranslator.ui.main.fragment.dialog.SelectDialog;
 import com.wiryaimd.mangatranslator.util.PermissionHelper;
+import com.wiryaimd.mangatranslator.util.RealPath;
 
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -49,21 +62,38 @@ public class MainActivity extends AppCompatActivity {
 //            } catch (IOException e) {
 //                e.printStackTrace();
 //            }
-            Glide.with(MainActivity.this).load(result).into(demoimg);
+//            Glide.with(MainActivity.this).load(result).into(demoimg);
         }
     });
 
     private ActivityResultLauncher<String> launcherPdf = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
         @Override
         public void onActivityResult(Uri result) {
-            File file = new File(result.getPath());
+            List<Bitmap> bitmapList = new ArrayList<>();
             try {
-                PDDocument document = PDDocument.loadNonSeq(file, null);
-                List<PDPage> pages = document.getDocumentCatalog().getAllPages();
+                File file = RealPath.from(MainActivity.this, result);
+                PdfRenderer pdfRenderer = new PdfRenderer(ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY));
+                Bitmap bitmap;
+                Log.d(TAG, "onActivityResult: count: " + pdfRenderer.getPageCount());
+                for (int i = 0; i < pdfRenderer.getPageCount(); i++) {
+                    PdfRenderer.Page page = pdfRenderer.openPage(i);
+                    int width = getResources().getDisplayMetrics().densityDpi / 72 * page.getWidth();
+                    int height = getResources().getDisplayMetrics().densityDpi / 72 * page.getHeight();
+                    bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+//                    Canvas canvas = new Canvas(bitmap);
+//                    canvas.drawColor(Color.WHITE);
+//                    canvas.drawBitmap(bitmap, 0, 0, null);
+//                    Rect r = new Rect(0, 0, width, height);
+//                    page.render(bitmap, r, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
+//                    bitmapList.add(bitmap);
+                    page.close();
+                }
+                pdfRenderer.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
+//            Glide.with(MainActivity.this).load(bitmapList.get(3)).into(demoimg);
+            Log.d(TAG, "onActivityResult: asuuuu");
         }
     });
 
@@ -86,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void openPdf() {
-                launcherPdf.launch("pdf/*");
+                launcherPdf.launch("application/pdf");
             }
         };
         mainViewModel.setOpenFile(openFile);
